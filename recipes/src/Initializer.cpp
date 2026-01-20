9a4d638a625fc80d60a51d0add656756a81a426e
Bartimaeus812 <bettsdan812@gmail.com>
2026-01-20 18:16:30 -0500

    Rearranged project into sections: recipe and map Seperated each section into bin - output files and executables bin/dat - archived data src - source files src/lib - library files added makefiles for both sections, assuming all .cpp files have a main()

---
+++ recipes/src/Initializer.cpp
@@ -0,0 +1,69 @@
+#include "lib/json.hpp"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using json = nlohmann::ordered_json;
+
+class Resource {
+    public:
+        Resource(string name_in, int amount_in) {
+            name = name_in;
+            amount = amount_in;
+        }
+        string get_name() {return name;}
+        int get_amount() {return amount;}
+    private:
+        string name;
+        int amount;
+};
+
+class recipe {
+    public:
+
+    private:
+        vector<Resource> inputs;
+        vector<Resource> outputs;
+        double speed;
+};
+
+void variableInitializer(string path);
+
+int main() {
+    string name_in = "names.json";
+    string recipe_in = "recipes.json";
+
+    variableInitializer(name_in);
+    variableInitializer(recipe_in);
+
+    ifstream fin(recipe_in);
+    ofstream fout("values.txt");
+
+    json data;
+    fin >> data;
+
+    int input;
+    double output;
+
+    for (const auto& block : data) {
+        input = stoi(data.value("ManufactoringDuration", ""));
+        output = 60/input;
+        fout << output << "\n";
+    }
+
+    fin.close();
+    fout.close();
+
+    return 0;
+}
+
+void variableInitializer(string path) {
+    ifstream fin(path);
+    
+    json data;
+    fin >> data;
+    
+    fin.close();
+}
