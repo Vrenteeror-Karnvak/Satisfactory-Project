8d08d2b82e3a67002331ec64886f2fdb5f7b1d5c
Vrenteeror-Karnvak <adam.beane.13@gmail.com>
2026-01-23 11:05:16 -0500

    Resource and Recipe Class
    Created the resource and recipe class. Both only have member functions for setting and extracting data from the variable.

--- Test File.cpp
+++ Test File.cpp
@@ -4,24 +4,30 @@
 #include <iostream>
 #include <fstream>
 
+#include <typeinfo>
+
 using namespace std;
 using json = nlohmann::ordered_json;
 
 int main(int argc, char* argv[]) {
-    string str1 = "Packaged";
-    string str2 = "Unpackage";
+    filesystem::path exePath = filesystem::absolute(argv[0]).parent_path();
 
-    if (str1.find("package") != string::npos) {
-        cout << str1.find("package") << endl;
-    }
-    else { 
-        cout << "\"package\" not found in \"Packaged\"." << endl;
+    // opens all of the input and output file streams
+    ifstream recipe_in(exePath / "recipes.json");
+    ofstream recipe_out(exePath / "recipes_ipm.json");
+
+    json recipe;
+    recipe_in >> recipe;
+
+    double value = 0;
+
+    for (auto& block : recipe) {
+        value = stod(block.value("ManufactoringDuration", ""));
+        block["ManufactoringDuration"] = to_string(60 / value);
     }
 
-    if (str2.find("package") != string::npos) {
-        cout << str2.find("package") << endl;
-    }
-    else { 
-        cout << "\"package\" not found in \"Unpackage\"." << endl;
-    }
+    recipe_out << recipe.dump(4);
+
+    recipe_in.close();
+    recipe_out.close();
 }
---
+++ recipe.cc
@@ -0,0 +1,93 @@
+#ifndef RECIPE_CC
+#define RECIPE_CC
+
+#include "recipe.h"
+
+#include "resource.h"
+
+using namespace std;
+using json = nlohmann::ordered_json;
+
+Recipe::Recipe() {
+    name = "N/A";
+    factory = "N/A";
+    ipm = 0;
+}
+
+Recipe::Recipe(const json& data) {
+    name = data.value("DisplayName", "");
+    factory = data.value("ProducedIn", "");
+    ipm = stod(data.value("ManufactoringDuration", ""));
+    
+    for (auto& ingredient : data["Ingredients"]) {
+        Resource stuff(ingredient);
+        ingredients.push_back(stuff);
+    }
+
+    for (auto& product : data["Product"]) {
+        Resource stuff(product);
+        products.push_back(stuff);
+    }
+}
+
+void Recipe::set_name(string title) {
+    name = title;
+}
+
+void Recipe::set_factory(string building) {
+    factory = building;
+}
+
+void Recipe::set_ipm(double rate) {
+    ipm = rate;
+}
+
+void Recipe::set_ingredients(vector<Resource> ingredient) {
+    for (int i = 0; i < ingredient.size(); i++) {
+        ingredients.push_back(ingredient.at(i));
+    }
+}
+
+void Recipe::add_ingredient(Resource ingredient) {
+    ingredients.push_back(ingredient);
+}
+
+void Recipe::set_products(vector<Resource> product) {
+    for (int i = 0; i < product.size(); i++) {
+        products.push_back(product.at(i));
+    }
+}
+
+void Recipe::add_product(Resource product) {
+    products.push_back(product);
+}
+
+string Recipe::get_name() {
+    return name;
+}
+
+string Recipe::get_factory() {
+    return factory;
+}
+
+double Recipe::get_ipm() {
+    return ipm;
+}
+
+vector<Resource> Recipe::get_ingredients() {
+    return ingredients;
+}
+
+Resource Recipe::get_ingredient(int i) {
+    return ingredients.at(i);
+}
+
+vector<Resource> Recipe::get_products() {
+    return products;
+}
+
+Resource Recipe::get_product(int i) {
+    return products.at(i);
+}
+
+#endif
